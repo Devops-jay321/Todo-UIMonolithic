b218f33d7fa6d7436ac21c4f817772cc037632ae
Jaydeepc1985 <jaydeep.chakraborty1985@gmail.com>
2026-02-02 11:56:06 +0530

    Update TodoApp.js
    Signed-off-by: Jaydeepc1985 <jaydeep.chakraborty1985@gmail.com>

--- src/TodoApp.js
+++ src/TodoApp.js
@@ -4,7 +4,7 @@ import { Button, TextField, Container, Typography, Grid, Card, CardContent, Icon
 import { Delete } from '@mui/icons-material';
 import { Box } from '@mui/material';
 
-const API_BASE_URL = 'http://20.204.35.242:8000/api';
+const API_BASE_URL = 'http://4.213.35.240:8000/api';
 
 const backgroundImage = process.env.PUBLIC_URL + '/background.jpg';
 
